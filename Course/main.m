ce13be84da1d69f8a8c1e04fb1086d7119966a5a
Ben Novikov <veniamin.novikov@gmail.com>
2015-07-22 01:55:04 +0300

    Initial Commit

---
+++ Course/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Course
+//
+//  Created by Admin on 15/07/22/.
+//  Copyright (c) 2015 BenNovikov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
