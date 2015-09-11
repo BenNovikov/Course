ac801064fc7861b4f12b7cdc9a5bd37d7efd4de1
Ben Novikov <veniamin.novikov@gmail.com>
2015-09-12 00:07:22 +0300

    UITests -> initial commit

---
+++ iOSProject/iOSProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSProject
+//
+//  Created by Admin on 15/09/11/.
+//  Copyright (c) 2015 BenNovikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSProject/iOSProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSProject
+//
+//  Created by Admin on 15/09/11/.
+//  Copyright (c) 2015 BenNovikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
